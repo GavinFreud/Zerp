1db3dc6b5e703d48fb2cfc233838d60f5a151278
gavinfreud <ramiyer1998@gmail.com>
2018-05-12 16:34:45 -0400

    Added stuff to UI. Need to work on game logic

--- ui.py
+++ ui.py
@@ -1,6 +1,43 @@
 import display
 
-def menu():
+
+# 'l' - Go left
+# 'r' - Go right
+# 'u' - Go up
+# 'd' - Go down
+# 'y/n' - Replies 'Yes' or 'No' when prompted
+# 'i' - quickly shows you your items
+# 'p' - shows you what's in the room'
+# 'c' - Brings up the control screen again
+
+def menu(room):
+    directions = ['r', 'l', 'u', 'd']
+    userin = ''
     while(1):
-        
-        
+        userin = input("Type a command or type . if you want to specify an item")
+        if userin == '.':
+            item()
+        elif userin == 'r':
+            if room.right != null:
+                break
+            else:
+                print("You can't go that way.\n")
+        elif userin == 'l':
+            if room.left != null:
+                break
+            else:
+                print("You can't go that way.\n")
+        elif userin == 'u':
+            if room.up != null:
+                break
+            else:
+                print("You can't go that way.\n")
+        elif userin == 'd':
+            if room.down != null:
+                break
+            else:
+                print("You can't go that way.\n")
+               
+        elif userin == 'i':
+            display.userItems(user)
+            return 
